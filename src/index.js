f2a77d3d1c3d72041d989e53f81a5cb8ed23911c
TheCandle <vbrajr@gmail.com>
2025-04-30 13:00:44 +0300

    Upgraded to Tailwind UI

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import DailyTracker from './DailyTracker';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DailyTracker />
+  </React.StrictMode>
+);
